d86e3cd6ad8a70937f31c8b46b0aa6a557b45e26
BartoszMilanowski <bartosz.milanowski@protonmail.com>
2022-04-07 12:56:47 +0200

    create project

---
+++ src/main/java/pl/coderslab/egrades/EGradesApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.egrades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EGradesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EGradesApplication.class, args);
+    }
+
+}
